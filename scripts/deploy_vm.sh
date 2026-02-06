6e13ac5dbb6e4f8a139ab6e02cb6ebd810c51300
MatMat511 <mathieu.barth@gmail.com>
2026-02-06 14:14:21 +0000

    modif du fichier deploy

--- scripts/deploy_vm.sh
+++ scripts/deploy_vm.sh
@@ -10,6 +10,7 @@ VM_NAME=$1
 IMAGE="Alpine_3.21"
 FLAVOR="Eval"
 NETWORK="LAN-LABO"
+USER-DATA="/var/snap/microstack/common/var/user-init.yaml"
 
 # Vérifie si la VM existe déjà
 if sudo microstack.openstack server list | grep -q "$VM_NAME"; then
@@ -22,8 +23,35 @@ sudo microstack.openstack server create \
  --image "$IMAGE" \
  --flavor "$FLAVOR" \
  --network "$NETWORK" \
+ --user-data "$USER-DATA" \
+ --config-drive true
  "$VM_NAME"
 
+# Attendre que la VM soit ACTIVE
+while true; do
+  STATUS=$(microstack.openstack server show "$VM_NAME" -f value -c status 2>/dev/null || echo "ERROR")
+  if [ "$STATUS" = "ACTIVE" ]; then
+    break
+  elif [ "$STATUS" = "ERROR" ]; then
+    echo "Erreur : la VM $VM_NAME est en état ERROR"
+    exit 1
+  else
+    echo "État actuel de la VM $VM_NAME : $STATUS, attente..."
+    sleep 5
+  fi
+done1~# Attendre que la VM soit ACTIVE
+while true; do
+  STATUS=$(microstack.openstack server show "$VM_NAME" -f value -c status 2>/dev/null || echo "ERROR")
+  if [ "$STATUS" = "ACTIVE" ]; then
+    break
+  elif [ "$STATUS" = "ERROR" ]; then
+    echo "Erreur : la VM $VM_NAME est en état ERROR"
+    exit 1
+  else
+    echo "État actuel de la VM $VM_NAME : $STATUS, attente..."
+    sleep 5
+  fi
+done
 
 #Cette partie ne marche pas à cause de la commande wait qui n'existe
 # Attente que la VM soit active et récupération de l'IP
